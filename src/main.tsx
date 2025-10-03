882812016d9c2982c3f9d8b779a12f3bfa4bdde5
Matsushima Toshihiro <s504062@iys.local>
2025-10-03 16:45:50 +0900

    setting enviroment of shadow cnui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter } from "react-router-dom";
+import './index.css';
+import './styles/globals.css';
+import { App } from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+     <BrowserRouter>
+      <App />
+     </BrowserRouter>
   </StrictMode>,
 )

0b3e73c654b7d6984c2adda7c83efc9429413fba
Matsushima Toshihiro <s504062@iys.local>
2025-10-06 17:09:04 +0900

    画面レイアウトの追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
-import './index.css';
 import './styles/globals.css';
-import { App } from './App.tsx';
+
+import  Home  from './pages/Home'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
      <BrowserRouter>
-      <App />
+      <Home />
      </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import  Header from './../components/Header';
+import { PicSlider } from './../components/PicSlider'
+
+export default function Home(){
+
+    return(
+        <div>
+            <Header />
+            <PicSlider />
+        </div>
+    );
+}
